854a69a0f7032cca12a55f1e42d8a88a16c09c8a
lcppembroke <lcppembroke@test.com>
2020-02-13 14:00:09 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Project Name
+>  Code from Net Ninja Vue JS 2 Tutorial episode 13 - simple punch bag game
+
+## General info
+This code was done as part of introducing myself to VueJS front end framework.
+
+## Technologies
+* Browser   
+
+## Setup
+* git clone https://github.com/lcmpembroke/netNinjaVue_13_punchbag.git
+
+In browser, open the .html files in directly in the browser
+file:///YourFilePath/netNinjaVue_13_punchbag/index.html
+
+
---
+++ app.js
@@ -0,0 +1,29 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        health: 100,
+        ended: false
+    },
+    methods: {
+        punch() {
+            this.health -= 10;
+            if (this.health <= 0) {
+                this.ended = true;
+            }
+        },
+        restart() {
+            this.health = 100;
+            this.ended = false;
+        },        
+       
+    },
+    computed: {
+        computedClasses() {
+            return {
+                available: this.available,
+                nearby: this.nearby
+            }
+        }
+
+    },
+});
